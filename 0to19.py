34cde0bae6c76145072faefeb959c2f7df7b32c7
shaurya1806 <24bcl008@sot.pdpu.ac.in>
2025-04-25 09:53:13 +0530

    Delete 0to19.py

--- 0to19.py
+++
@@ -1,9 +0,0 @@
-def number_to_word(num):
-    words = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
-    if 0 <= num <= 19:
-        return words[num]
-    else:
-        return "Invalid number"
-
-num = int(input("Enter a number between 0 and 19: "))
-print(number_to_word(num))
